4ee2f0786045e84fe8b533fe9773e965ef4588f7
liusaprian <aprianlius@gmail.com>
2022-01-06 17:26:30 +0700

    fix transactioncontroller

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -9,24 +9,17 @@ import view.TransactionReportView;
 public class TransactionController {
 
 	private static TransactionController controller = null;
-<<<<<<< HEAD
-	private CustomerTransactionView customerView = CustomerTransactionView.getInstance();
-	private TransactionController() {}
-=======
 	private Transaction transaction;
 	private CustomerTransactionView view = CustomerTransactionView.getInstance();
 	
 	private TransactionController() {
 		transaction = new Transaction();
 	}
->>>>>>> ca95875fbe8c0d31dbdc28bac5d2112acce2f882
 
 	public static TransactionController getInstance() {
 		if(controller == null) controller = new TransactionController();
 		return controller;
 	}
-<<<<<<< HEAD
-=======
 	
 	public Vector<Transaction> getAllTransaction() {
 		return transaction.getAllTransactions();
@@ -39,5 +32,4 @@ public class TransactionController {
 	public void returnAdminTransactionReportView() {
 		new TransactionReportView();
 	}
->>>>>>> ca95875fbe8c0d31dbdc28bac5d2112acce2f882
 }
